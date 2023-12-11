cfa4b2378a4eb44007deb276bbd08bb75040e9d1
Karine <karinedeveloper@gmail.com>
2023-12-11 20:00:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+import { EstilosGlobais } from "./components/EstilosGlobais";
+
+const FundoGradiente = styled.div`
+  background: var(
+    --Gradiente-fundo,
+    linear-gradient(175deg, #041833 4.16%, #04244f 48%, #154580 96.76%)
+  );
+  width: 100%;
+  min-height: 100vh;
+`;
+
+export const App = () => {
+  return <FundoGradiente>
+    <EstilosGlobais/>
+  </FundoGradiente>;
+};
